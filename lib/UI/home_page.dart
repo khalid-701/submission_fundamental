c3c79f204af95e7aa397d7d639fe46599d2228ef
khalid701 <khalidramdhan97@gmail.com>
2021-11-23 18:27:29 +0700

    detail resto berhasil ges

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -2,12 +2,13 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:submission_fundamental/UI/resto_detail_page.dart';
 import 'package:submission_fundamental/blue_print/restoran_model.dart';
 
-
 class HomePage extends StatefulWidget {
   static const routeName = '/home';
+
   const HomePage({Key? key}) : super(key: key);
 
   @override
@@ -17,50 +18,111 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Restaurant"),
-      ),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Restaurant"),
+        ),
 
-      //ListView.builder(itemBuilder: (context, index))
+        //ListView.builder(itemBuilder: (context, index))
 
-      body: FutureBuilder<dynamic>(
-        future: DefaultAssetBundle.of(context)
-            .loadString('assets/local_restaurant.json'),
-        builder: (context, snapshot) {
-          final List<Resto> resto = parseData(snapshot.data);
-          return resto.length != 0 ? ListView.builder(
-            itemCount: resto.length,
-              itemBuilder: (context, index) {
-            return _buildItems(context, resto[index]);
-          }) :
-              Center(child: Text("data tidak ada"),);
-
-        },
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Restaurant",
+              style: GoogleFonts.lato(
+                textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Text(
+              "Recommendation for you!",
+              style: GoogleFonts.lato(
+                textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(height: 25,),
+            Expanded(
+              child: FutureBuilder<dynamic>(
+                future: DefaultAssetBundle.of(context)
+                    .loadString('assets/local_restaurant.json'),
+                builder: (context, snapshot) {
+                  final List<Resto> resto = parseData(snapshot.data);
+                  return resto.isNotEmpty
+                      ? ListView.builder(
+                          itemCount: resto.length,
+                          itemBuilder: (context, index) {
+                            return _buildItems(context, resto[index]);
+                          })
+                      : Center(
+                          child: Text("No data found", style: GoogleFonts.lato(
+                            textStyle: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold),
+                          )),
+                        );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
   Widget _buildItems(BuildContext context, Resto data) {
     return ListTile(
-      onTap: (){
-        print("klik");
+      onTap: () {
         Navigator.pushNamed(context, RestoDetail.routeName, arguments: data);
       },
       leading: Hero(
         tag: data.pictureId.toString(),
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(
-            minWidth: 44,
-            minHeight: 44,
-            maxWidth: 66,
-            maxHeight: 66,
-          ),
-          child: Image.network(data.pictureId.toString(), fit: BoxFit.cover),
+        child: SizedBox(
+          width: 100,
+          height: 80,
+          child: Image.network("${data.pictureId}", fit: BoxFit.cover),
         ),
       ),
-      title: Text(data.name.toString()),
-      subtitle: Text("${data.city.toString()}\n ${data.rating}"),
+      title: Text("${data.name}", style: GoogleFonts.lato(
+        textStyle: const TextStyle(
+            color: Colors.black,
+            fontSize: 14,
+            fontWeight: FontWeight.bold),
+      )),
+      subtitle: RichText(
+        text: TextSpan(
+          children: [
+            const WidgetSpan(
+              child: Icon(Icons.location_on_outlined, size: 14),
+            ),
+            TextSpan(
+                text: "${data.city} \n\n", style: GoogleFonts.lato(
+              textStyle: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 13,
+              ),
+            )),
+            const WidgetSpan(
+              child: Icon(Icons.star, size: 14),
+            ),
+            TextSpan(
+                text: "${data.rating}", style: GoogleFonts.lato(
+              textStyle: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold),
+            )),
+          ],
+        ),
+      ),
+      //Text("${data.city.toString()}\n ${data.rating}"),
       isThreeLine: true,
     );
   }
--- lib/UI/resto_detail_page.dart
+++ lib/UI/resto_detail_page.dart
@@ -1,25 +1,150 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:submission_fundamental/blue_print/restoran_model.dart';
 
 class RestoDetail extends StatelessWidget {
   static const routeName = '/resto_detail';
   final Resto resto;
-  const RestoDetail({Key? key,required this.resto}) : super(key: key);
+
+  const RestoDetail({Key? key, required this.resto}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Resto Detail"),
-      ),
-      body: Column(
-        children: [
-          Hero(
-            tag: resto.pictureId.toString(),
-              child: Image.network(resto.pictureId.toString(), width: MediaQuery.of(context).size.width, height: 300,)),
-          Text(resto.name.toString()),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(resto.name.toString()),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Hero(
+                    tag: resto.pictureId.toString(),
+                    child: Image.network(
+                      resto.pictureId.toString(),
+                      width: MediaQuery.of(context).size.width,
+                      height: 250,
+                    fit: BoxFit.cover,)),
 
-        ],
+                const SizedBox(height: 20,),
+                RichText(
+                  text: TextSpan(
+                    children: [
+                     const WidgetSpan(
+                        child: Icon(Icons.location_on_outlined, size: 16),
+                      ),
+                      TextSpan(
+                        text: "${resto.city}", style: GoogleFonts.lato(
+                        textStyle: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      )
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 20,),
+                 Text("Descriptions", style: GoogleFonts.lato(
+                   textStyle: const TextStyle(
+                       color: Colors.black,
+                       fontSize: 14,
+                       fontWeight: FontWeight.w700),
+                 )),
+                const SizedBox(height: 10,),
+                Text("${resto.description}", style: GoogleFonts.lato(
+                  textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                  )),),
+                const SizedBox(height: 10,),
+                Text("Foods", style: GoogleFonts.lato(
+                  textStyle: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                      fontWeight: FontWeight.w700
+                  ),
+                )),
+
+                const SizedBox(height: 5,),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 150,
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: resto.menus!.foods!.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SizedBox(
+                              width: 150,
+                              height: 100,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Align(alignment: Alignment.center,
+                                    child: Image.network(
+                                        "https://cdn.icon-icons.com/icons2/881/PNG/512/Hamburger_icon-icons.com_68741.png", width: 100,),
+                                  ),
+                                  SizedBox(height: 10,),
+                                  Text(resto.menus!.foods![index].name.toString()),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                ),
+                const SizedBox(height: 10,),
+                Text("Drinks", style: GoogleFonts.lato(
+                  textStyle: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w700
+                  ),
+                )),
+
+                const SizedBox(height: 5,),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 150,
+                  child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: resto.menus!.drinks!.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: SizedBox(
+                              width: 150,
+                              height: 100,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Align(alignment: Alignment.center,
+                                    child: Image.network(
+                                      "https://cdn.icon-icons.com/icons2/709/PNG/512/Drinks-46_icon-icons.com_61977.png", width: 100,),
+                                  ),
+                                  SizedBox(height: 10,),
+                                  Text(resto.menus!.drinks![index].name.toString()),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
